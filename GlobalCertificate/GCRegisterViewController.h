b7e7f8882a63557129b913df8c18614d64de9125
pspzx <superrockman2009@163.com>
2013-09-03 13:21:37 +0800

    Add the registerViewController

---
+++ GlobalCertificate/GCRegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCRegisterViewController.h
+//  GlobalCertificate
+//
+//  Created by JL on 13-9-3.
+//  Copyright (c) 2013年 ninetowns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCRegisterViewController : UIViewController
+
+@end
---
+++ GlobalCertificate/GCRegisterViewController.m
@@ -0,0 +1,38 @@
+//
+//  GCRegisterViewController.m
+//  GlobalCertificate
+//
+//  Created by JL on 13-9-3.
+//  Copyright (c) 2013年 ninetowns. All rights reserved.
+//
+
+#import "GCRegisterViewController.h"
+
+@interface GCRegisterViewController ()
+
+@end
+
+@implementation GCRegisterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
